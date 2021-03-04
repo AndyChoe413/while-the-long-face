b19e563f1cba550bbdd2695287dc7a3c2ec1797d
Andy Choe <Andrew.s.choe@gmail.com>
2021-03-04 15:44:58 -0500

    completed assignment

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 function numberDoubler(num) {
     // Your code here
+    while (num < 100) {
+       num *= 2
+    }
+    return num
 }
 
 function stringRepeater(str) {
     // Your code here
+    let newStr = str;
+    while (newStr.length < 10) {
+    newStr += str
+    }
+    return newStr
 }
+
 
 function makeDivisible(x, y) {
     // Your code here
+    let result = x;
+    while (result % y !== 0) {
+        result++
+    }
+    return result
 }
 
 module.exports = {
